09f66f64d49163c22c5a1494e4ad2001bd3dc1c6
wuyadong1990 <1021778375@qq.com>
2022-06-14 14:24:07 +0800

    Update server.go

--- server.go
+++ server.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Route => handler
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!\n")
+		return c.String(http.StatusOK, "Hello, World V2!\n")
 	})
 
 	// Start server
